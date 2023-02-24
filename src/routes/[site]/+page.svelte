2f31835d699aab0df05c8133ba9614601d142306
micheal <bumanlee9@gmail.com>
2023-02-24 03:25:16 +0100

    Add primo basic config

---
+++ src/routes/[site]/+page.svelte
@@ -0,0 +1,5 @@
+<script>
+  import Page from '@primo-app/primo/views/editor/Page.svelte'
+</script>
+
+<Page id="index" />
---
+++ src/routes/[site]/[...page]/+page.svelte
@@ -0,0 +1,6 @@
+<script>
+  import Page from '@primo-app/primo/views/editor/Page.svelte'
+  import { page } from '$app/stores'
+</script>
+
+<Page id={$page.params.page} />
